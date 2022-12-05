b9486183165c44b9827c5a53760ec939911b7fa7
andrewpaper <118001300+andrewpaper@users.noreply.github.com>
2022-12-05 15:10:10 +0100

    Update lib.h

--- lib.h
+++ lib.h
@@ -1,8 +1,8 @@
 #ifndef ESERCITAZIONE7_LEGNO_H
 #define ESERCITAZIONE7_LEGNO_H
 
-bool verifica(char);
-char conversione(char);
+bool verifica(char a);
+char conversione(char a);
 
 #endif //ESERCITAZIONE7_LEGNO_H
 
