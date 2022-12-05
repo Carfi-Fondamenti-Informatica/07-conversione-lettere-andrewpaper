24d7af60e1e8aa173f538f520b5f78024cd9ca4b
andrewpaper <118001300+andrewpaper@users.noreply.github.com>
2022-12-05 15:07:56 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,21 @@
+#include "legno.h"
 
+bool verifica(char a){
+    int b;
+    b=(int)a;
+    if((b>=65 && b<=90) || (b>=97 && b<=122))
+        return true;
+    else
+        return false;
+}
+char conversione(char a){
+    int b;
+    b=(int)a;
+    int c=0;
+    if(b>=65 && b<=90)
+        c=b+32;
+    else if(b>=97 && b<=122)
+        c=b-32;
+    return c;
+
+}
